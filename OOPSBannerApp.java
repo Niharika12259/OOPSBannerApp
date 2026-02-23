016fa7f90c3d5a02f8a5b73628385872d0ae35e5
Niharika12259 <nj0455@srmist.edu.in>
2026-02-23 08:47:30 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp prints OOPS text in console
+ * 
+ * @author Niharika12259
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
